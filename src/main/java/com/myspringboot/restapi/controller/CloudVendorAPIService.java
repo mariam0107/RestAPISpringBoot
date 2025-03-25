cf34a20725b3f88b1c246dae5f297c9c5dedb5e5
mariam george <mariam0107@gmail.com>
2025-03-25 06:43:19 +0100

    Added controller functionality,tested via Postman

--- src/main/java/com/myspringboot/restapi/controller/CloudVendorAPIService.java
+++ src/main/java/com/myspringboot/restapi/controller/CloudVendorAPIService.java
@@ -1,15 +1,30 @@
 package com.myspringboot.restapi.controller;
 
 import com.myspringboot.restapi.model.CloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudvendor")
-public class CloudAPIService {
+public class CloudVendorAPIService {
+    CloudVendor cloudVendor;
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails(String vendorId){
-        return new CloudVendor("C1","Vendor 1","Address 123","12345678")
+        return cloudVendor;
+                // new CloudVendor("C1","Vendor 1","Address 123","12345678");
+    }
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor created successfully";
+    }
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated successfully";
+    }
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted successfully";
     }
 }
